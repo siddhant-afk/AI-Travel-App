b653247dea95e0ddfdb3c7aa7a983267809f0cae
siddhant-afk <siddhantdudhane21@gmail.com>
2025-08-11 20:32:09 +0530

    Add PopularCitiesList component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./_components/Hero";
 import Image from "next/image";
+import { PopularCitiesList } from "./_components/PopularCitiesList";
 
 export default function Home() {
   return (
   
   <div>
   <Hero />
+  <PopularCitiesList />
   </div>
   );
 }

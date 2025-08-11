5d510ab0a5eda0d2780ca2b9b6ec464cf10d415a
siddhant-afk <siddhantdudhane21@gmail.com>
2025-08-11 18:58:22 +0530

    Add Header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit} from "next/font/google";
 import "./globals.css";
+import Provider from "./provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const outfit = Outfit({
   subsets: ["latin"],
+ 
 });
 
 export const metadata: Metadata = {
@@ -25,9 +22,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      className={outfit.className}
+       
       >
+        <Provider>
+
         {children}
+        </Provider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
   
   <div>
-    Hello World
+
   </div>
   );
 }
